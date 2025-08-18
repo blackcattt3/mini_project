bf4ab6c7ff61b36b5e1eb97e65f5e519df333cdf
Jaehee Roh <snowcat3b@gmail.com>
2025-08-18 21:56:13 +0900

    to do list

---
+++ to_do_list/main.js
@@ -0,0 +1,34 @@
+function newRegister(){
+    let newList = document.createElement("li");
+    newList.setAttribute("class", "items");
+    let itemList = document.querySelector("#item-list");
+    let item = document.querySelector("#item");
+    let newText = document.createTextNode(item.value);
+    newList.appendChild(newText);
+    item.value = "";
+    item.focus();
+
+    let newS = document.createElement("span");
+    let delText = document.createTextNode("X");
+    newS.setAttribute("class", "del");
+    newS.appendChild(delText);
+    newList.appendChild(newS);
+    itemList.insertBefore(newList, itemList.children[0]);
+
+    let delBtn = document.querySelectorAll(".del");
+    for(let i=0;i<delBtn.length;i++){
+        delBtn[i].addEventListener("click", function(){
+            if(this.parentNode.parentNode){
+                this.parentNode.parentNode.removeChild(this.parentNode);
+            }
+        })
+    }
+
+    let colorText = document.querySelectorAll(".items");
+    for(let i=0;i<colorText.length;i++){
+        colorText[i].addEventListener("click", function(){
+            this.style.color = "#ccc";
+        })
+    }
+
+}
