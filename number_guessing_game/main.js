2e8499f9318f70d535a8b28d599adc2c5af793a1
Jaehee Roh <snowcat3b@gmail.com>
2025-08-23 22:36:51 +0900

    test

--- number_guessing_game/main.js
+++ number_guessing_game/main.js
@@ -2,6 +2,7 @@
 // 기회는 총 다섯번
 // 결과창에 1)up, down, 1~100까지의 숫자를 입력해주세요
 // 리셋버튼
+// 배열안에 숫자 저장하면서 만약 배열안에 이미 배열안에 있던 숫자면 "이미 입력한 값입니다" 출력
 
 let answerVal = Math.floor(Math.random()*100 + 1);
 let inputVal = document.getElementById("input-value");
@@ -10,6 +11,7 @@ let submitBtn = document.getElementById("submit-btn");
 let resultShow = document.getElementById("result");
 let resetBtn = document.getElementById("reset-btn");
 let chance = 5;
+let items = [];
 
 submitBtn.addEventListener("click", play);
 resetBtn.addEventListener("click", reset);
@@ -48,3 +50,13 @@ function reset(){
     chanceShow.innerHTML = "게임을 시작합니다!";
     resultShow.innerHTML = "결과가 표시됩니다!";
 }
+
+function addNum(a){
+    if(items.includes(a)){
+        resultShow.innerHTML = "이미 입력한 값입니다.";
+    } else{
+        items.push(a);
+    }
+}
+
+hello
